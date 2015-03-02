396cea651afd9b2e5f050dd50a2eea9018091558
lukino563 <lukino563@gmail.com>
2015-03-02 22:23:59 +0100

    kernel: merge my modified kernel source 3.4.106

---
+++ include/generated_BCK/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Feb 14 18:00:03 CET 2015"
+#define LINUX_COMPILE_BY "lukino563"
+#define LINUX_COMPILE_HOST "UbuntuHP"
+#define LINUX_COMPILER "gcc version 4.9.3 20141031 (prerelease) (crosstool-NG linaro-1.13.1+bzr2709 - Linaro GCC 2014.11) "
