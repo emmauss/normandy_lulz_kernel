c40b2dfb847c9621102c5be8bf01607dde1fb83f
lukino563 <lukino563@gmail.com>
2015-03-12 15:11:51 +0100

    dm-crypt: remove intelli_plug performance boost optimization

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -997,10 +997,6 @@ static void kcryptd_io_write(struct dm_crypt_io *io)
 	generic_make_request(clone);
 }
 
-#ifdef CONFIG_INTELLI_PLUG
-extern void intelli_plug_perf_boost(bool);
-#endif
-
 static void kcryptd_io(struct work_struct *work)
 {
 	struct dm_crypt_io *io = container_of(work, struct dm_crypt_io, work);
@@ -1012,9 +1008,6 @@ static void kcryptd_io(struct work_struct *work)
 		crypt_dec_pending(io);
 	} else
 		kcryptd_io_write(io);
-#ifdef CONFIG_INTELLI_PLUG
-		intelli_plug_perf_boost(false);
-#endif
 }
 
 static void kcryptd_queue_io(struct dm_crypt_io *io)
@@ -1663,9 +1656,6 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 		ti->error = "Couldn't create kcryptd queue";
 		goto bad;
 	}
-#ifdef CONFIG_INTELLI_PLUG
-	intelli_plug_perf_boost(true);
-#endif
 
 	ti->num_flush_requests = 1;
 	ti->discard_zeroes_data_unsupported = 1;
